f7bd589d3b45fa26b97e7212c9096a552587bb3c
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2023-11-21 17:10:10 -0400

    Resolução exercicio 15 - JS

---
+++ Exercicio15/app.js
@@ -0,0 +1,92 @@
+/*
+  01
+
+  - Obtenha a ul do index.html e adicione em todos os elementos filhos dela,  
+    uma classe 'video';
+  - Exiba no console os elementos filhos da ul com a classe já inserida.
+*/
+const ul = document.querySelector("ul");
+Array.from(ul.children).forEach((element) => {
+  element.classList.add("video");
+  console.log(element);
+});
+/*
+  02
+
+  - Usando a propriedade adequada, descubra quem é o elemento pai do h2
+    e exiba-o no console;
+*/
+const title = document.querySelector("h2");
+console.log(title.parentElement);
+
+/*
+  03
+
+  - Descubra quem é o próximo elemento irmão do h1 e exiba-o no console;
+*/
+console.log(title.nextElementSibling);
+/*
+  04
+
+  - Descubra quem é o irmão anterior da ul e exiba-o no console;
+*/
+
+console.log(title.previousElementSibling);
+
+/*
+  05
+
+  - Quando um clique acontecer em alguma das lis, faça com que a li clicada seja  
+    exibida no console.
+*/
+const lis = document.querySelectorAll("li");
+lis.forEach((li) => {
+  li.addEventListener("click", (event) => {
+    console.log(event.target);
+  });
+});
+/*
+  06
+
+  - Quando o botão for clicado, adicione o nome dos vídeos abaixo dentro da ul;
+  - Cada nome deve estar dentro de uma li.
+*/
+
+const videos = [
+  {
+    name: "Como o promise all funciona | JavaScript",
+    length: "00:01:52",
+  },
+  {
+    name: "Como refatorar um for loop | JavaScript",
+    length: "00:04:18",
+  },
+  {
+    name: "Como fazer requisições HTTP com o método fetch | JavaScript",
+    length: "00:02:55",
+  },
+];
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  videos.forEach((video) => {
+    const li = document.createElement("li");
+    li.textContent = `${video.name} - ${video.length}`;
+    ul.append(li);
+  });
+});
+/*
+  07
+
+  - Se um clique no h1 acontecer, faça com que todos os elementos dentro do body 
+    sejam removidos.
+*/
+
+const titleH1 = document.querySelector("h1");
+
+titleH1.addEventListener("click", () => {
+  const bodyElements = document.querySelectorAll("body *");
+  bodyElements.forEach((element) => {
+    element.remove();
+  });
+});
