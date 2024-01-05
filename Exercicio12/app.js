1408f6a764ad4fcf18a0c328ffc409515daa9482
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-01-05 07:05:04 -0400

    Resolucao Finalizado - Exercicio 12 - Objetos em Arrays e Objeto Math - JS

--- Exercicio12/app.js
+++ Exercicio12/app.js
@@ -27,6 +27,16 @@ const best2019Movies = [
   { title: "A Vida Invisível", directedBy: "Karim Aïnouz" },
 ];
 
+let message = "Segundo o site Omelete, os melhores filmes de 2019 são:";
+
+console.log("Segundo o site Omelete, os melhores filmes de 2019 são:");
+best2019Movies.forEach((element) => {
+  message += `
+   - ${element.title}, dirigido ${element.directedBy}`;
+});
+
+console.log(message);
+
 /*
   02
 
@@ -64,13 +74,24 @@ const youtubeUser = {
     },
     country: "Brasil",
   },
+
+  msgVideo() {
+    console.log(`Vídeos recentes de ${this.name}:`);
+    this.videos.recentVideos.forEach((element) => {
+      console.log(element.title);
+    });
+  },
 };
+
+youtubeUser.msgVideo();
 
 /*
   03
 
   - Exiba o valor do PI no console.
 */
+
+console.log(Math.PI);
 
 /*
   04
@@ -80,6 +101,7 @@ const youtubeUser = {
 */
 
 const firstNumber = 8.3;
+console.log(Math.ceil(firstNumber));
 
 /*
   05
@@ -89,6 +111,7 @@ const firstNumber = 8.3;
 */
 
 const secondNumber = 3.5;
+console.log(Math.round(secondNumber));
 
 /*
   06
@@ -98,6 +121,7 @@ const secondNumber = 3.5;
 */
 
 const thirdNumber = 8.9;
+console.log(Math.floor(thirdNumber));
 
 /*
   07
@@ -106,6 +130,7 @@ const thirdNumber = 8.9;
 */
 
 const fourthNumber = 5.5;
+console.log(Math.trunc(fourthNumber));
 
 /*
   08
@@ -113,3 +138,5 @@ const fourthNumber = 5.5;
   - A cada vez que o index.html for carregado, exiba no console um número  
     aleatório de 0 à 10, incluindo 0 e 10.
 */
+const randomNumber = Math.random();
+console.log(Math.round(randomNumber * 10));
