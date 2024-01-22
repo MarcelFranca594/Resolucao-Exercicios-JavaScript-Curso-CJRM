548fe9a65cc9d8da614b93d829d895363cf4a03f
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-01-22 08:57:46 -0400

    Resolucao Exercicio - Map, filter e reduce - JS

--- Exercicio21/app.js
+++ Exercicio21/app.js
@@ -5,7 +5,9 @@
     o novo array no console.
 */
 
-const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
+const randomNumbers = [10, 30, 15, 25, 50, 40, 5];
+const numbersImpar = randomNumbers.filter((item) => item % 2 === 1);
+console.log(numbersImpar);
 
 /*
   02
@@ -13,7 +15,17 @@ const randomNumbers = [10, 30, 15, 25, 50, 40, 5]
   - Exiba no console quantos números abaixo de 501 o array abaixo possui.
 */
 
-const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
+const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691];
+
+const numbersAbaixo50 = crazyNumbers.reduce((acc, item) => {
+  if (item < 501) {
+    acc += 1;
+  }
+
+  return acc;
+}, 0);
+
+console.log(numbersAbaixo50);
 
 /*
   03
@@ -22,7 +34,10 @@ const crazyNumbers = [937, 5, 395, 402, 501, 333, 502, 781, 3, 691]
     exiba o novo array no console.
 */
 
-const numbers = [5, 7, 3]
+const numbers = [5, 7, 3];
+
+const numbersQuadrado = numbers.map((item) => item ** 2);
+console.log(numbersQuadrado);
 
 /*
   04
@@ -36,18 +51,24 @@ const numbers = [5, 7, 3]
 */
 
 const cart = [
-  { name: 'Dark Souls III', price: 95.03 },
-  { name: 'Shadow of the Tomb Raider', price: 101.19 },
-  { name: 'Sekiro: Shadows Die Twice', price: 179.99 },
-  { name: 'Resident Evil 2', price: 119.90 },
-  { name: 'Death Stranding', price: 149.99 }
-]
+  { name: "Dark Souls III", price: 95.03 },
+  { name: "Shadow of the Tomb Raider", price: 101.19 },
+  { name: "Sekiro: Shadows Die Twice", price: 179.99 },
+  { name: "Resident Evil 2", price: 119.9 },
+  { name: "Death Stranding", price: 149.99 },
+];
 
 /*
   - Nome 1
   - Nome 2
   - Nome 3
 */
+
+const listaNome = cart.reduce((acc, produt) => {
+  return `${acc}- ${produt.name}\n`;
+}, "");
+
+console.log(listaNome);
 
 /*
   05
@@ -58,17 +79,20 @@ const cart = [
 */
 
 const tarantinoMovies = [
-  { name: 'Bastardos inglórios', release: 2009 },
-  { name: 'Pulp Fiction', release: 1994 },
-  { name: 'Kill Bill: Volume 2', release: 2004 },
-  { name: 'Quatro Quartos', release: 1995 },
-  { name: 'Sin City', release: 2005 },
-  { name: 'Era uma Vez em... Hollywood', release: 2019 },
-  { name: 'Django Livre', release: 2012 },
-  { name: 'Cães de Aluguel', release: 1992 },
-  { name: 'À Prova de Morte', release: 2007 },
-  { name: 'Kill Bill: Volume 1', release: 2003 }
-]
+  { name: "Bastardos inglórios", release: 2009 },
+  { name: "Pulp Fiction", release: 1994 },
+  { name: "Kill Bill: Volume 2", release: 2004 },
+  { name: "Quatro Quartos", release: 1995 },
+  { name: "Sin City", release: 2005 },
+  { name: "Era uma Vez em... Hollywood", release: 2019 },
+  { name: "Django Livre", release: 2012 },
+  { name: "Cães de Aluguel", release: 1992 },
+  { name: "À Prova de Morte", release: 2007 },
+  { name: "Kill Bill: Volume 1", release: 2003 },
+];
+
+const filmesAntes2000 = tarantinoMovies.filter((item) => item.release < 2000);
+console.log(filmesAntes2000);
 
 /*
   06
@@ -78,18 +102,15 @@ const tarantinoMovies = [
 */
 
 const tvShows = [
-  { name: 'Breaking Bad', releaseYear: 2008 },
-  { name: 'Mr. Robot', releaseYear: 2015 },
-  { name: 'True Detective', releaseYear: 2014 },
-  { name: 'Hannibal', releaseYear: 2013 },
-  { name: 'The Handmaid\'s Tale', releaseYear: 2017 },
-  { name: 'House M.D.', releaseYear: 2004 },
-  { name: 'Watchmen', releaseYear: 2019 }
-]
+  { name: "Breaking Bad", releaseYear: 2008 },
+  { name: "Mr. Robot", releaseYear: 2015 },
+  { name: "True Detective", releaseYear: 2014 },
+  { name: "Hannibal", releaseYear: 2013 },
+  { name: "The Handmaid's Tale", releaseYear: 2017 },
+  { name: "House M.D.", releaseYear: 2004 },
+  { name: "Watchmen", releaseYear: 2019 },
+];
 
-/*
-  07
+const nomesSeries = tvShows.map((item) => item.name);
 
-  - Observe os loops da sua versão do quiz e considere se, baseado no que foi  
-    visto nessa aula, você deve refatorá-los.
-*/
+console.log(nomesSeries);
