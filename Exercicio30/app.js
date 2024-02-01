4d5d52f8e0501c16775de12c82992492ef2c7e53
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-02-01 11:27:11 -0400

    Exercicio 30 -JS

---
+++ Exercicio30/app.js
@@ -0,0 +1,9 @@
+/*
+  01
+
+  - Usando promises, faça um request para este endpoint:
+    https://jsonplaceholder.typicode.com/users
+  - Se o request estiver ok, exiba os objetos no console;
+  - Se o request não estiver ok, exiba no console "Não foi possível obter os 
+    dados dos usuários."
+*/
