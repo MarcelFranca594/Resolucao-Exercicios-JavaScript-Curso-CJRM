164b9b81cfa2a993df1bd15f42c762ceba25e634
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-02-02 09:21:30 -0400

    Exercicio 31 - JS

---
+++ Exercicio31/app.js
@@ -0,0 +1,9 @@
+/*
+  01
+
+  - Utilizando a fetch API, implemente uma função assíncrona que busca dados do 
+    seu perfil de usuário no GitHub, através do seguinte endpoint:
+    - https://api.github.com/users/SEU_NOME_DE_USUARIO_NO_GITHUB
+  - Implemente uma segunda função que exibe, no console, seus dados de usuário 
+    do GitHub.
+*/
