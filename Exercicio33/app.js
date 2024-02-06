5715998a7bf5c675880a118fb9e631116bf91046
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-02-06 16:02:57 -0400

    Exercicio 33 - JS

---
+++ Exercicio33/app.js
@@ -0,0 +1,12 @@
+/*
+  01
+
+  - O idioma da interface gráfica da aplicação que estamos construindo 
+    (Weather App) é pt-BR;
+  - Ao invocarmos a getCityWeather, observe que a propriedade "WeatherText" do 
+    objeto obtido armazena a descrição do clima em inglês: "cloudy", "rain", 
+    etc;
+  - Traduza os possíveis valores dessa propriedade;
+  - Não é necessário implementar condicionais, objetos, ou arrays para fazer
+    isso.
+*/
