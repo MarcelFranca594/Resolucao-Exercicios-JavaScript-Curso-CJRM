478ba2faa3e804a583a9f6329751bbdba9ddd6c9
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-02-09 10:18:48 -0400

    Resolução Finalizado exericicio 37 sobre POO em JavaScript

--- Exercicio37/app.js
+++ Exercicio37/app.js
@@ -6,19 +6,21 @@
 */
 
 class Animal {
-  constructor (name) {
-    this.name = name
+  constructor(name) {
+    this.name = name;
   }
 }
 
 class Rabbit extends Animal {
-  constructor (name) {
-    this.name = name
-    this.created = new Date()
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.created = new Date();
   }
 }
 
-// let rabbit = new Rabbit('White Rabbit')
+let rabbit = new Rabbit("White Rabbit");
+console.log(rabbit);
 
 /*
   02
@@ -27,11 +29,24 @@ class Rabbit extends Animal {
     funcione.
 */
 
-// const counter = new Counter()
+class Counter {
+  constructor() {
+    this.valorAtual = 0;
+  }
+  getValue() {
+    return this.valorAtual;
+  }
 
-// counter.getValue()
-// counter.increment()
-// counter.getValue()
+  increment() {
+    this.valorAtual++;
+  }
+}
+
+const counter = new Counter();
+
+console.log(counter.getValue());
+counter.increment();
+console.log(counter.getValue());
 
 /*
   03
@@ -41,14 +56,9 @@ class Rabbit extends Animal {
   - Não invoque o construtor.
 */
 
-const values = [
-  0,
-  {},
-  '',
-  [],
-  NaN,
-  () => {}
-]
+const values = [0, {}, "", [], NaN, () => {}];
+const truthyValues = values.filter(Boolean);
+console.log(truthyValues);
 
 /*
   04
@@ -60,98 +70,61 @@ const values = [
     funcione.
 */
 
-// class Clock {
-//   constructor ({ template }) {
-//     this.template = template
-//   }
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
 
-//   render () {
-//     const date = new Date()
-//     let hours = date.getHours()
-//     let minutes = date.getMonth()
-//     let seconds = date.getSeconds()
+  render() {
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMonth();
+    let seconds = date.getSeconds();
 
-//     if (hours < 10) {
-//       hours = `0${hours}`
-//     }
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
 
-//     if (minutes < 10) {
-//       minutes = `0${minutes}`
-//     }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
 
-//     if (seconds < 10) {
-//       seconds = `0${seconds}`
-//     }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
 
-//     const formattedTime = this.template
-//       .replace('h', hours)
-//       .replace('m', minutes)
-//       .replace('s', seconds)
+    const formattedTime = this.template
+      .replace("h", hours)
+      .replace("m", minutes)
+      .replace("s", seconds);
 
-//     console.log(formattedTime)
-//   }
+    console.log(formattedTime);
+  }
 
-//   start () {
-//     this.render()
-//     this.timer = setInterval(() => this.render(), 1000)
-//   }
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
 
-//   stop () {
-//     clearInterval(this.timer)
-//   }
-// }
+  stop() {
+    clearInterval(this.timer);
+  }
+}
 
-// class ExtendedClock extends Clock {
-//   constructor ({ options }) {
-//     super(options)
-    
-//     let { precision = 1000 } = options
-//     this.precision = precision
-//   }
+class ExtendedClock extends Clock {
+  constructor(options) {
+    super(options);
 
-//   start () {
-//     this.render()
-//     this.timer = setInterval(() => this.render(), this.precision)
-//   }
-// }
+    let { precision = 1000 } = options;
+    this.precision = precision;
+  }
 
-// const clock = ExtendedClock({ template: 'h:m:s', precision: 1000 })
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+}
 
-// clock.start()
+const clock = new ExtendedClock({ template: "h:m:s", precision: 1000 });
 
-/*
-  05
-
-  - No index.html há um elemento "textarea" e um parágrafo. A cada vez que um 
-    caractere for inserido no textarea, exiba no parágrafo a quantidade de 
-    caracteres que o textarea contém.
-*/
-
-
-
-/*
-  06
-
-  - Já implementamos os métodos forEach, some, map e filter, do zero;
-  - Neste exercício, seu desafio será criar, do zero, o método reduce;
-  - Implemente uma função "reduce" que possui a mesma funcionalidade do método 
-    reduce original;
-  - Você não poderá utilizar o método reduce original, embutido na linguagem;
-  - A assinatura e retorno da invocação desta função devem ser os seguintes:
-    - reduce([1, 2, 3], (acc, item) => acc + item, 0) // 6;
-    - reduce([2, 3, 4], (acc, item) => acc + item, 0) // 9;
-    - reduce(
-        [1, 2],
-        (acc, item) => {
-          acc['number-' + item] = item
-          return acc
-        },
-        {}
-      ) // {"number-1": 1, "number-2": 2};
-    - reduce([1, 2], (acc, item, index) => acc + index, 0) // 1;
-    - reduce([1, 2], (acc, item, index, array) => acc + array[index], 0) // 3;
-  - Utilize os casos de uso acima para testar sua função;
-  - Se você não se lembra como o método reduce funciona, deixarei abaixo do 
-    vídeo de correção dos exercícios um link para a aula de introdução ao 
-    reduce e um link para a documentação do método no MDN.
-*/
+clock.start();
